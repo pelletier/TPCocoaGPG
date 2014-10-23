74a844c808e9d9fdb6f722fc6348c012ecb6780f
Thomas Pelletier <pelletier.thomas@gmail.com>
2014-10-23 17:54:28 +0200

    Initial Commit

---
+++ TPCocoaGPG/TPCocoaGPG.h
@@ -0,0 +1,13 @@
+//
+//  TPCocoaGPG.h
+//  TPCocoaGPG
+//
+//  Created by Thomas Pelletier on 10/23/14.
+//  Copyright (c) 2014 Thomas Pelletier. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TPCocoaGPG : NSObject
+
+@end
---
+++ TPCocoaGPG/TPCocoaGPG.m
@@ -0,0 +1,13 @@
+//
+//  TPCocoaGPG.m
+//  TPCocoaGPG
+//
+//  Created by Thomas Pelletier on 10/23/14.
+//  Copyright (c) 2014 Thomas Pelletier. All rights reserved.
+//
+
+#import "TPCocoaGPG.h"
+
+@implementation TPCocoaGPG
+
+@end
