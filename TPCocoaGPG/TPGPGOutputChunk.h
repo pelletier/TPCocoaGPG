b0a445b9ced67a104890053624bcfeb11b804df2
Thomas Pelletier <pelletier.thomas@gmail.com>
2015-02-11 18:00:27 +0100

    Add some doc for output chunk

--- TPCocoaGPG/TPGPGOutputChunk.h
+++ TPCocoaGPG/TPGPGOutputChunk.h
@@ -8,11 +8,21 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ Internal representation of GPG's parsed standard error.
+ */
 @interface TPGPGOutputChunk : NSObject {
   NSString* _key;
   NSString* _text;
 }
 
+/**
+ Allocate and initialize a new chunk.
+ 
+ @param key Output key
+ @param text Associated text
+ @return The new chunk
+*/
 + (TPGPGOutputChunk*)makeWithKey:(NSString*)key andText:(NSString*)text;
 
 @property (nonatomic, assign) NSString* key;
